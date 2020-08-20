fe794970c7229c64a226a01210a13ad91d01020b
Maksim <Iammaksim2000shirshikov@gmail.com>
2020-08-20 12:36:53 +0300

    projects

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 new WOW().init();
+
+var mySwiper = new Swiper('.swiper-container', {
+   pagination: {
+      el: '.projects-pagination',
+      bulletClass: 'project-bullet',
+      bulletActiveClass: 'project-bullet--active',
+      clickable: true,
+      autoplay: {
+         delay: 7000,
+      },
+      speed: 2800,
+   },
+})
+
+mySwiper.autoplay.start();
